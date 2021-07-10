2cd4c7b12bb24ddfcae999f95ef0fa2c33775c7d
Wade Wilson <84837136+Sayandhridolickid@users.noreply.github.com>
2021-07-10 15:21:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"{file_name} \n @Movievibezz,
+                caption = "@Movievibezz" ,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
