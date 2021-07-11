dac4a1560bc9696d4fbbac4bec3a066c5576e47c
Wade Wilson <84837136+Sayandhridolickid@users.noreply.github.com>
2021-07-11 14:02:48 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,16 +5,17 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<b><i><u>I am °°𝙽𝚎𝚐𝚊𝚗​§... 🤨<u><i><b>
+
+<b><i>I am °°𝙽𝚎𝚐𝚊𝚗​§... 🤨<i><b>
 
 So, You can't add me to your group.. 😉
 
 For More Details Click Help Button Below..
-@Movievibezz
+<i>@Movievibezz</i>
 </b>"""    
     
     HELP_TEXT = """
-<b><i><u>For Queries!?</u></i></b>
+<b><i>For Queries!?</i></b>
 
 <b>@Ridolickid</b>
 
@@ -25,9 +26,9 @@ For More Details Click Help Button Below..
     
 <b>➥ Creator</b> : <b><i><a href="https://t.me/Ridolickid">Ꮗ Ꭿ ⅅ ℰ Ꮗ ℐ ℒ Ѕ Ꮎ ℕ</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ Language</b> : 🔐
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ Library</b> : <i>🔐</i>
 
 <b>➥ Source Code</b> : <i><a href="https://github.com/Sayandhridolickid/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
