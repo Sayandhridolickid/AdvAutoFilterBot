a91e0a7981876919a7a86a84296c3a4f72ad4749
Wade Wilson <84837136+Sayandhridolickid@users.noreply.github.com>
2021-07-11 20:47:40 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,7 +147,7 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Go to Next Page⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("Go to Next Page👉", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Channel- @Mv_Main..Found {(len_results)} Results For Your Request: <code>{query}</code>",
+                text=f"𝐆𝐫𝐨𝐮𝐩:- @Movievibezz..Result Found {(len_results)} Your Request: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
