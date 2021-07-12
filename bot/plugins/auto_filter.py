f50748fb678582fe8c9925ecdfe8d221dcd29008
Wade Wilson <84837136+Sayandhridolickid@users.noreply.github.com>
2021-07-12 21:51:47 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -201,14 +201,13 @@ async def auto_filter(bot, update):
         reply_markup = InlineKeyboardMarkup(result[0])
 
         try:
-            await bot.send_photo(
-    chat_id=update.chat.id,
-    photo="https://telegra.ph/file/31ddbcdd6176cf8ca0a94.jpg"
-    caption=f"Found {(len_results)} Result For Your etc....."
-    reply_markup=reply_markup,
-    parse_mode="html",
-    reply_to_mesaage_id=update.message_id
-)
+await bot.send_message(
+                chat_id = update.chat.id,
+                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                reply_markup=reply_markup,
+                parse_mode="html",
+                reply_to_message_id=update.message_id
+            )
         except ButtonDataInvalid:
             print(result[0])
         
